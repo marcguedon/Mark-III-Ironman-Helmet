159663bc8fd5f30b15d2d06c99c423fdfbd972ef
Marc GUEDON <marcguedon@gmail.com>
2024-12-08 13:31:28 +0100

    improve code

--- IronManHelmetV1/IronManHelmetV1.ino
+++ IronManHelmetV1/IronManHelmetV1.ino
@@ -8,101 +8,95 @@
 
 #define EYES_PIN          3
 #define TACTILE_KEY_PIN   2
+#define HELMET_SERVO_PIN  5
+#define FACE_SERVO_PIN    6
+
+// MAY NEED TO CHANGE
 #define OPENED_HELMET     0
 #define OPENED_FACE       0
 #define CLOSED_HELMET     155
 #define CLOSED_FACE       175
-#define HELMET_SERVO_PIN  5
-#define FACE_SERVO_PIN    6
 #define SERVOS_SPEED      190
 
-ServoEasing HelmetServo;
-ServoEasing FaceServo;
+ServoEasing helmet_servo;
+ServoEasing face_servo;
 
-bool helmetOpen;
-bool eyesLit;
+bool helmet_open = false;
+bool eyes_lit = true;
 
-void setup() {
-  pinMode(EYES_PIN, OUTPUT);
-  pinMode(TACTILE_KEY_PIN, INPUT);
+void setup()
+{
+    pinMode(EYES_PIN, OUTPUT);
+    pinMode(TACTILE_KEY_PIN, INPUT);
 
-  //Initialisation of servomotors
-  HelmetServo.attach(HELMET_SERVO_PIN);
-  FaceServo.attach(FACE_SERVO_PIN);
-  setSpeedForAllServos(SERVOS_SPEED);
-  HelmetServo.setEasingType(EASE_CUBIC_IN_OUT);
-  FaceServo.setEasingType(EASE_CUBIC_IN_OUT);
+    // Initialisation of servomotors
+    helmet_servo.attach(HELMET_SERVO_PIN);
+    face_servo.attach(FACE_SERVO_PIN);
+    setSpeedForAllServos(SERVOS_SPEED);
+    helmet_servo.setEasingType(EASE_CUBIC_IN_OUT);
+    face_servo.setEasingType(EASE_CUBIC_IN_OUT);
 
-  //Initialisation of variables
-  helmetOpen = false;
-  eyesLit = true;
-
-  //Functions performing the actions
-  helmetFunction();
-  eyesFunction();
+    // Intialisation of the helmet and eyes
+    helmet_function();
+    eyes_function();
 }
 
-void loop() {
-  //If contact on the left side 
-  if(digitalRead(TACTILE_KEY_PIN) == HIGH){
-    //If helmet opened, helmet and eyes state change
-    if(helmetOpen == true){
-      helmetOpen = false;
-      eyesLit = true;
-    }
-    //If helmet closed, helmet and eyes state change 
-    else{
-      helmetOpen = true;
-      eyesLit = false;
-    }
-    
-    //Functions performing the actions
-    helmetFunction();
-    eyesFunction();
+void loop()
+{
+    if(digitalRead(TACTILE_KEY_PIN) == HIGH) // Contact on the left side (touch sensor) 
+    {
+        eyes_lit = helmet_open;
+        helmet_open = !helmet_open;
 
-    //While contact on the left side
-    while(digitalRead(TACTILE_KEY_PIN) == HIGH){}
-  }
+        helmet_function();
+        eyes_function();
+
+        while(digitalRead(TACTILE_KEY_PIN) == HIGH) {}
+    }
 }
 
 //Eyes on/off function
-void eyesFunction(){
-  //If eyes on
-  if(eyesLit == true){
-    analogWrite(EYES_PIN, 255);
-    delay(10);
-    analogWrite(EYES_PIN, 0);
-    delay(500);
-    analogWrite(EYES_PIN, 255);
-    delay(50);
-    analogWrite(EYES_PIN, 0);
-    delay(75);
-    analogWrite(EYES_PIN, 255);
-    delay(200);
-    analogWrite(EYES_PIN, 0);
-    delay(50);
-    for(int i = 32; i < 255; i++){
-      analogWrite(EYES_PIN, i);
-      delay(8);
+void eyes_function()
+{
+    if(eyes_lit == true)
+    {
+        analogWrite(EYES_PIN, 255);
+        delay(10);
+        analogWrite(EYES_PIN, 0);
+        delay(500);
+        analogWrite(EYES_PIN, 255);
+        delay(50);
+        analogWrite(EYES_PIN, 0);
+        delay(75);
+        analogWrite(EYES_PIN, 255);
+        delay(200);
+        analogWrite(EYES_PIN, 0);
+        delay(50);
+
+        for(int i = 32; i < 255; i++)
+        {
+            analogWrite(EYES_PIN, i);
+            delay(8);
+        }
     }
-  }
-  //If eyes off
-  else{
-    analogWrite(EYES_PIN, 0);
-  }
+    
+    else analogWrite(EYES_PIN, 0);
 }
 
 //Helmet open/close function
-void helmetFunction(){
-  //If helmet opened
-  if(helmetOpen == true){
-    HelmetServo.setEaseTo(OPENED_HELMET);
-    FaceServo.setEaseTo(OPENED_FACE);
-  }
-  //If helmet closed
-  else{
-    HelmetServo.setEaseTo(CLOSED_HELMET);
-    FaceServo.setEaseTo(CLOSED_FACE);
-  }
-  synchronizeAllServosStartAndWaitForAllServosToStop();
+void helmet_function()
+{
+    if(helmet_open == true)
+    {
+        helmet_servo.setEaseTo(OPENED_HELMET);
+        face_servo.setEaseTo(OPENED_FACE);
+    }
+    
+    else
+    {
+        helmet_servo.setEaseTo(CLOSED_HELMET);
+        face_servo.setEaseTo(CLOSED_FACE);
+    }
+
+    synchronizeAllServosStartAndWaitForAllServosToStop();
 }
--- IronManHelmetV2/IronManHelmetV2.ino
+++ IronManHelmetV2/IronManHelmetV2.ino
@@ -7,236 +7,221 @@
 #include <ServoEasing.hpp>
 #include <VoiceRecognitionV4.h>
 
-//PINS NUMBER
 #define EYES_PIN          3
 #define TACTILE_KEY_PIN   2
 #define HELMET_SERVO_PIN  5
 #define FACE_SERVO_PIN    6
 
-//MAY NEED TO CHANGE
+// MAY NEED TO CHANGE
 #define OPENED_HELMET     0
 #define OPENED_FACE       0
 #define CLOSED_HELMET     150
 #define CLOSED_FACE       170
 #define SERVOS_SPEED      190
 
-//COMMANDS NUMBER
+// COMMANDS NUMBER
 #define JarvisRecord      (0)
 #define HelmetOnRecord    (1)
 #define HelmetOffRecord   (2)
 #define EyesOnRecord      (3)
 #define EyesOffRecord     (4)
 
-ServoEasing HelmetServo;
-ServoEasing FaceServo;
-
+ServoEasing helmet_servo;
+ServoEasing face_servo;
 VR myVR1(&Serial1);
 
-bool helmetOpen = false;
-bool eyesLit = true;
-bool eyesAlreadyOn = false;
-
+bool helmet_open = false;
+bool eyes_lit = true;
+bool eyes_already_on = false;
 uint8_t buf[64];
 
-void setup() {
-  pinMode(EYES_PIN, OUTPUT);
-  pinMode(TACTILE_KEY_PIN, INPUT);
+void setup()
+{
+    pinMode(EYES_PIN, OUTPUT);
+    pinMode(TACTILE_KEY_PIN, INPUT);
 
-  Serial1.begin(9600);
+    Serial1.begin(9600);
 
-  //Initialisation of servomotors
-  HelmetServo.attach(HELMET_SERVO_PIN);
-  FaceServo.attach(FACE_SERVO_PIN);
-  setSpeedForAllServos(SERVOS_SPEED);
-  HelmetServo.setEasingType(EASE_CUBIC_IN_OUT);
-  FaceServo.setEasingType(EASE_CUBIC_IN_OUT);
+    //Initialisation of servomotors
+    helmet_servo.attach(HELMET_SERVO_PIN);
+    face_servo.attach(FACE_SERVO_PIN);
+    setSpeedForAllServos(SERVOS_SPEED);
+    helmet_servo.setEasingType(EASE_CUBIC_IN_OUT);
+    face_servo.setEasingType(EASE_CUBIC_IN_OUT);
 
-//NOT NECESSARY / TO ENSURE PROPER OPERATION
-////////////////////////////////////////////////////////////////////////////////////////////////////
-  Serial.begin(115200);
-  Serial.println("Elechouse Voice Recognition V3 Module\r\nControl LED sample");
-    
-  if(myVR1.clear() == 0){
-    Serial.println("Recognizer cleared.");
-  }else{
-    Serial.println("Not find VoiceRecognitionModule.");
-    Serial.println("Please check connection and restart Arduino.");
-    while(1);
-  }
-  
-  if(myVR1.load((uint8_t)JarvisRecord) >= 0){
-    Serial.println("JarvisRecord loaded");
-  }
-  
-  if(myVR1.load((uint8_t)HelmetOnRecord) >= 0){
-    Serial.println("HelmetOnRecord loaded");
-  }
+// NOT NECESSARY / TO ENSURE PROPER OPERATION
+    // Serial.begin(115200);
+    // Serial.println("Elechouse Voice Recognition V3 Module\r\nControl LED sample");
 
-  if(myVR1.load((uint8_t)HelmetOffRecord) >= 0){
-    Serial.println("HelmetOffRecord loaded");
-  }
+    // if(myVR1.clear() == 0)
+    //     Serial.println("Recognizer cleared.");
 
-  if(myVR1.load((uint8_t)EyesOnRecord) >= 0){
-    Serial.println("EyesOnRecord loaded");
-  }
+    // else
+    // {
+    //     Serial.println("Not find VoiceRecognitionModule.");
+    //     Serial.println("Please check connection and restart Arduino.");
+    //     while(1);
+    // }
 
-  if(myVR1.load((uint8_t)EyesOffRecord) >= 0){
-    Serial.println("EyesOffRecord loaded");
-  }
-////////////////////////////////////////////////////////////////////////////////////////////////////
+    // if(myVR1.load((uint8_t)JarvisRecord) >= 0)
+    //     Serial.println("JarvisRecord loaded");
 
+    // if(myVR1.load((uint8_t)HelmetOnRecord) >= 0)
+    //     Serial.println("HelmetOnRecord loaded");
 
-  //Functions performing the actions
-  helmetFunction();
-  eyesFunction();
+    // if(myVR1.load((uint8_t)HelmetOffRecord) >= 0)
+    //     Serial.println("HelmetOffRecord loaded");
+
+    // if(myVR1.load((uint8_t)EyesOnRecord) >= 0)
+    //     Serial.println("EyesOnRecord loaded");
+
+    // if(myVR1.load((uint8_t)EyesOffRecord) >= 0)
+    //     Serial.println("EyesOffRecord loaded");
+
+    // Intialisation of the helmet and eyes
+    helmet_function();
+    eyes_function();
 }
 
-void loop() {
-  //If contact on the left side 
-  if(digitalRead(TACTILE_KEY_PIN) == HIGH){
-    //If helmet opened, helmet and eyes state change
-    if(helmetOpen == true){
-      helmetOpen = false;
-      eyesLit = true;
-    }
-    //If helmet closed, helmet and eyes state change 
-    else{
-      helmetOpen = true;
-      eyesLit = false;
-    }
+void loop()
+{
     
-    //Functions performing the actions
-    helmetFunction();
-    eyesFunction();
+    if(digitalRead(TACTILE_KEY_PIN) == HIGH) // Contact on the left side 
+    {
+        eyes_lit = helmet_open;
+        helmet_open = !helmet_open;
 
-    //While contact on the left side
-    while(digitalRead(TACTILE_KEY_PIN) == HIGH){}
-  }
+        helmet_function();
+        eyes_function();
 
-  int ret;
-  ret = myVR1.recognize(buf, 50);
+        while(digitalRead(TACTILE_KEY_PIN) == HIGH) {}
+    }
   
-  //If a recognized command
-  if(ret>0){
-    //Recognize the command and act
-    switch(buf[1]){
-      case JarvisRecord:
-        break;
-      case HelmetOnRecord:
-        helmetOpen = true;
-        eyesLit = false;
-        break;
-      case HelmetOffRecord:
-        helmetOpen = false;
-        eyesLit = true;
-        break;
-      case EyesOnRecord:
-        eyesLit = true;
-        break;
-      case EyesOffRecord:
-        eyesLit = false;
-        break;
-      default:
-        break;
-    }
-    
-    printVR(buf);
+    if(myVR1.recognize(buf, 50) > 0) // A command has been recognized
+    {
+        switch(buf[1])
+        {
+            case JarvisRecord:
+                break;
+            case HelmetOnRecord:
+                helmet_open = true;
+                eyes_lit = false;
+                break;
+            case HelmetOffRecord:
+                helmet_open = false;
+                eyes_lit = true;
+                break;
+            case EyesOnRecord:
+                eyes_lit = true;
+                break;
+            case EyesOffRecord:
+                eyes_lit = false;
+                break;
+            default:
+                break;
+        }
 
-    //Functions performing the actions
-    helmetFunction();
-    eyesFunction();
-  }
+        printVR(buf);
+
+        helmet_function();
+        eyes_function();
+    }
 }
 
 //Eyes on/off function
-void eyesFunction(){
-  //If eyes on
-  if(eyesLit == true){
-    //If not already lit
-    if(eyesAlreadyOn == false){
-      analogWrite(EYES_PIN, 255);
-      delay(10);
-      analogWrite(EYES_PIN, 0);
-      delay(500);
-      analogWrite(EYES_PIN, 255);
-      delay(50);
-      analogWrite(EYES_PIN, 0);
-      delay(75);
-      analogWrite(EYES_PIN, 255);
-      delay(200);
-      analogWrite(EYES_PIN, 0);
-      delay(50);
-      for(int i = 32; i < 255; i++){
-        analogWrite(EYES_PIN, i);
-        delay(8);
-      }
-      eyesAlreadyOn = true;
+void eyes_function()
+{
+    if(eyes_lit == true && eyes_already_on == false) // If not already lit
+    {
+        analogWrite(EYES_PIN, 255);
+        delay(10);
+        analogWrite(EYES_PIN, 0);
+        delay(500);
+        analogWrite(EYES_PIN, 255);
+        delay(50);
+        analogWrite(EYES_PIN, 0);
+        delay(75);
+        analogWrite(EYES_PIN, 255);
+        delay(200);
+        analogWrite(EYES_PIN, 0);
+        delay(50);
+
+        for(int i = 32; i < 255; i++)
+        {
+            analogWrite(EYES_PIN, i);
+            delay(8);
+        }
+
+        eyes_already_on = true;
     }
-  }
-  //If eyes off
-  else{
-    analogWrite(EYES_PIN, 0);
-    eyesAlreadyOn = false;
-  }
+  
+    else if(eyes_lit == false)
+    {
+        analogWrite(EYES_PIN, 0);
+        eyes_already_on = false;
+    }
 }
 
 //Helmet open/close function
-void helmetFunction(){
-  //If helmet opened
-  if(helmetOpen == true){
-    HelmetServo.setEaseTo(OPENED_HELMET);
-    FaceServo.setEaseTo(OPENED_FACE);
-  }
-  //If helmet closed
-  else{
-    HelmetServo.setEaseTo(CLOSED_HELMET);
-    FaceServo.setEaseTo(CLOSED_FACE);
-  }
-  synchronizeAllServosStartAndWaitForAllServosToStop();
+void helmet_function()
+{
+    if(helmet_open == true)
+    {
+        helmet_servo.setEaseTo(OPENED_HELMET);
+        face_servo.setEaseTo(OPENED_FACE);
+    }
+    
+    else
+    {
+        helmet_servo.setEaseTo(CLOSED_HELMET);
+        face_servo.setEaseTo(CLOSED_FACE);
+    }
+    
+    synchronizeAllServosStartAndWaitForAllServosToStop();
 }
 
 void printSignature(uint8_t *buf, int len)
 {
-  int i;
-  for(i=0; i<len; i++){
-    if(buf[i]>0x19 && buf[i]<0x7F){
-      Serial.write(buf[i]);
+    for(int i = 0; i < len; i++)
+    {
+        if(buf[i]>0x19 && buf[i]<0x7F)
+            Serial.write(buf[i]);
+
+        else
+        {
+            Serial.print("[");
+            Serial.print(buf[i], HEX);
+            Serial.print("]");
+        }
     }
-    else{
-      Serial.print("[");
-      Serial.print(buf[i], HEX);
-      Serial.print("]");
-    }
-  }
 }
 
 void printVR(uint8_t *buf)
 {
-  Serial.println("VR Index\tGroup\tRecordNum\tSignature");
+    Serial.println("VR Index\tGroup\tRecordNum\tSignature");
+    Serial.print(buf[2], DEC);
+    Serial.print("\t\t");
 
-  Serial.print(buf[2], DEC);
-  Serial.print("\t\t");
+    if(buf[0] == 0xFF) Serial.print("NONE");
 
-  if(buf[0] == 0xFF){
-    Serial.print("NONE");
-  }
-  else if(buf[0]&0x80){
-    Serial.print("UG ");
-    Serial.print(buf[0]&(~0x80), DEC);
-  }
-  else{
-    Serial.print("SG ");
-    Serial.print(buf[0], DEC);
-  }
-  Serial.print("\t");
+    else if(buf[0] & 0x80)
+    {
+        Serial.print("UG ");
+        Serial.print(buf[0] & (~0x80), DEC);
+    }
 
-  Serial.print(buf[1], DEC);
-  Serial.print("\t\t");
-  if(buf[3]>0){
-    printSignature(buf+4, buf[3]);
-  }
-  else{
-    Serial.print("NONE");
-  }
-  Serial.println("\r\n");
+    else
+    {
+        Serial.print("SG ");
+        Serial.print(buf[0], DEC);
+    }
+
+    Serial.print("\t");
+    Serial.print(buf[1], DEC);
+    Serial.print("\t\t");
+
+    if(buf[3]>0) printSignature(buf + 4, buf[3]);
+    else Serial.print("NONE");
+    
+    Serial.println("\r\n");
 }
